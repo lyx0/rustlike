6870833d2c840d50b088650c0ac2dc4825d5a067
lyx0 <66651385+lyx0@users.noreply.github.com>
2022-05-19 22:27:33 +0200

    init commit

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use rltk::{GameState, Rltk};
 
 struct State {}
-
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
+        // clear screen
         ctx.cls();
         ctx.print(1, 1, "Hello Rust World");
     }
 }
+
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
+    // We're setting a new variable named gs (game state) as a copy
+    // of the `State` struct.
     let gs = State {};
     rltk::main_loop(context, gs)
 }
