8a04dc69e07be3866d13156b95ac7c61eab5b83d
lyx0 <66651385+lyx0@users.noreply.github.com>
2022-05-23 17:56:19 +0200

    create an ECS and register components

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
-use rltk::{GameState, Rltk};
+#![allow(unused)]
 
-struct State {}
-impl GameState for State {
-    fn tick(&mut self, ctx: &mut Rltk) {
-        // clear screen
-        ctx.cls();
-        ctx.print(1, 1, "Hello Rust World");
-    }
+use rltk::{GameState, Rltk, VirtualKeyCode, RGB};
+use specs::prelude::*;
+use specs_derive::Component;
+use std::cmp::{max, min};
+
+// The derive macro brings a lot of boilerplate for the Component
+// so we don't have to repeat us over and over again.
+// For example:
+// `impl Component for Position` and for Renderable and so on.
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
 }
 
-fn main() -> rltk::BError {
-    use rltk::RltkBuilder;
-    let context = RltkBuilder::simple80x50()
-        .with_title("Roguelike Tutorial")
-        .build()?;
-    // We're setting a new variable named gs (game state) as a copy
-    // of the `State` struct.
-    let gs = State {};
-    rltk::main_loop(context, gs)
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+// A world is an ECS (Entity Control System). It gathers the data for each
+// entity or component and does something with it.
+struct State {
+    ecs: World,
+}
+
+fn main() {
+    let mut gs = State { ecs: World::new() };
+    // Here we tell our `World` to take a look at everything we gave it, namely
+    // everything that implements a `Component`, and create storages for those.
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
 }
