0de35b1b3abcdf5899f987bdaaade97e383dcc32
lyx0 <66651385+lyx0@users.noreply.github.com>
2022-05-27 20:18:11 +0200

    add arrow movement keys too

--- src/main.rs
+++ src/main.rs
@@ -52,10 +52,17 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
     match ctx.key {
         None => {}
         Some(key) => match key {
+            // Vim bindings
             VirtualKeyCode::H => try_move_player(-1, 0, &mut gs.ecs),
             VirtualKeyCode::L => try_move_player(1, 0, &mut gs.ecs),
             VirtualKeyCode::K => try_move_player(0, -1, &mut gs.ecs),
             VirtualKeyCode::J => try_move_player(0, 1, &mut gs.ecs),
+
+            // Arrow keys
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
             _ => {}
         },
     }
