d624bf820c390a1ef1be8e29ac3b1462f60183e0
lyx0 <66651385+lyx0@users.noreply.github.com>
2022-05-28 21:30:31 +0200

    start adding a map

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,45 @@ struct State {
     ecs: World,
 }
 
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall,
+    Floor,
+}
+
+// xy_idx returns a `vec` of tiles that represent a simple map.
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    // Multiples the `y` position by the map width (80) and adds `x`.
+    // This guarantees one tile per location.
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80 * 50];
+
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     // We need write access to the Position and the Player structures.
     let mut positions = ecs.write_storage::<Position>();
